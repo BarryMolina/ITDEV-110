1483d0029d908c9a348acc3f97a2a98ff0b052e4
Barry Molina <bazzaboy@gmail.com>
2019-09-15 11:08:58 -0500

    add textbook examples

---
+++ textbook_examples/ch2/Columbus.java
@@ -0,0 +1,5 @@
+public class Columbus {
+    public static void main(String[] args) {
+        System.out.println("In 1492 Columbus saild the ocean \rblue");
+    }
+}
---
+++ textbook_examples/ch2/Letters.java
@@ -0,0 +1,19 @@
+public class Letters {
+
+    public static void main(String[] args) {
+
+        char letter;
+        String myvar = "some string";
+
+        letter = 'A';
+        System.out.println(letter);
+        letter = 'B';
+        System.out.println(letter);
+
+        System.out.println(myvar);
+
+        myvar = letter;
+        System.out.println(myvar);
+
+    }
+}
