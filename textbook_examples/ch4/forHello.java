79ed6571fe0870ae1531fef4bf6dde2cdc922982
Barry Molina <bazzaboy@gmail.com>
2019-10-15 10:24:45 -0500

    forhello

---
+++ textbook_examples/ch4/forHello.java
@@ -0,0 +1,11 @@
+public class forHello {
+
+    public static void main(String[] args) {
+
+        int count;
+
+        for (count = 1; count <= 5; count++)
+            System.out.println("Hello");
+
+    }
+}
