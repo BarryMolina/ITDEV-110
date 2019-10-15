4c1417c97cf7b15086c141381c9f2f5f76914ee6
BarrMolina <bazzaboy@gmail.com>
2019-10-15 11:43:21 -0500

    do some examples

---
+++ textbook_examples/ch4/Fifth.java
@@ -0,0 +1,10 @@
+public class Fifth {
+
+    public static void main(String[] args) {
+
+        for (int x = 0; x <= 100; x +=5) {
+            System.out.println(x);
+        }
+
+    }
+}
--- textbook_examples/ch4/Squares.java
+++ textbook_examples/ch4/Squares.java
@@ -1,13 +1,21 @@
+import java.util.Scanner; 
+
 public class Squares {
 
     public static void main(String[] args) {
 
         int number;
+        int maxValue;
+        
+        Scanner keyboard = new Scanner(System.in); 
+
+        System.out.println("How high should I go? ");
+        maxValue = keyboard.nextInt();
 
         System.out.println("Number Number Squared");
         System.out.println("----------------------");
 
-        for (number = 1; number <= 10; number++) {
+        for (number = 1; number <= maxValue; number++) {
             System.out.println(number + "\t\t" + number * number);
         }
 
