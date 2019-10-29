523d154e9ef8ea6193bc4d9ea8d80f13422d442e
Barry Molina <bazzaboy@gmail.com>
2019-10-28 22:36:48 -0500

    add arrays

---
+++ textbook_examples/ch4/FileReadDemo.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.Scanner;
+public class FileReadDemo {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("Enter the name of a file: ");
+        String filename = keyboard.nextLine();
+
+        File file = new File(filename);
+        Scanner inputFile = new Scanner(file);
+
+        while (inputFile.hasNext()) {
+            String friendName = inputFile.nextLine();
+            System.out.println(friendName);
+        }
+
+        inputFile.close();
+    }
+}
---
+++ textbook_examples/ch4/ReadFirstLine.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.Scanner;
+public class ReadFirstLine {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("Enter the name of a file: ");
+        String filename = keyboard.nextLine();
+
+        File file = new File(filename);
+        Scanner inputFile = new Scanner(file);
+
+        String line = inputFile.nextLine();
+        System.out.println("The first line in the file is: ");
+        System.out.println(line);
+
+        inputFile.close();
+    }
+}
