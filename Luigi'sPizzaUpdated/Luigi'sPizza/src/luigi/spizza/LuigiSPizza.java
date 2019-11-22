d3a1712c8c01dc5cf39779185b9f17444e7d6cb8
BarrMolina <bazzaboy@gmail.com>
2019-11-21 20:28:15 -0600

    add names to pizzaapp

---
+++ Luigi'sPizzaUpdated/Luigi'sPizza/src/luigi/spizza/LuigiSPizza.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package luigi.spizza;
+
+/**
+ *
+ * Barry Molina, Kristen Klubertanz, Carlos Mayorga
+ * ITDEV-110 Pizza App Week 9 Team Project
+ */
+public class LuigiSPizza {
+
+
+    
+    public static void main(String[] args)
+    {
+		Order o = new Order();
+		o.takeOrder();
+    }
+    
+}
---
+++ Luigi'sPizzaUpdated/Luigi'sPizza/src/luigi/spizza/Pizza.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package luigi.spizza;
+
+import java.text.DecimalFormat;
+/**
+ *
+ * @author student
+ */
+public class Pizza {
+        int pizzaNum;
+		String toppings = "";
+		int numToppings = 0;
+        char sizeChar;
+        String sizeString;
+		double price = 0;
+        DecimalFormat df = new DecimalFormat("0.00");
+
+    public Pizza(int num) {
+        pizzaNum = num;
+    }
+    public int getPizzaNum() {
+        return pizzaNum;
+    }
+	public String getSize() {
+		return sizeString;
+	}
+    public void setSize(char size) {
+		sizeChar = size;
+		switch (size){
+			case 'S': sizeString = "Small";
+			break;
+			case 'M': sizeString = "Medium";
+			break;
+			case 'L': sizeString = "Large";
+			break;
+		}
+    }
+	public int getNumToppings() {
+		return numToppings;
+	}
+	public void addTopping(String topping) {
+		toppings += topping + " ";
+		numToppings++;
+	}
+	public void updatePrice(double amount) {
+		price += amount;
+	}
+	public double getPrice() {
+		return price;
+	}
+	@Override
+	public String toString() {
+		return "Info for pizza number " + pizzaNum + ":" +
+				"\n\t Size: " + sizeString +
+				"\n\t Toppings: " + toppings + 
+				"\n\t Total price for this pizza: $" + (df.format(price));
+	}
+}
